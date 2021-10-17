567dbd204983b84b398c15fe6514cb04a6ec12ff
Aruzhan <91003354+assylbekovna03@users.noreply.github.com>
2021-10-17 19:10:53 +0500

    Add files via upload
    adding position sticky when scrolling page header stays, and also mobile adaptation(without header) is added

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ document.getElementById("button").addEventListener("click",function(){
 document.querySelector(".close").addEventListener("click",function(){
     document.querySelector(".popup").style.display = "none";
 })
+document.getElementById("button").addEventListener("click",function(){
+    document.querySelector(".header").style.display = "none";
+})
+document.querySelector(".close").addEventListener("click",function(){
+    document.querySelector(".header").style.display = "flex";
+})
+
+// function search_word() {
+//     let input = document.getElementById('form1').value
+//     input=input.toLowerCase();
+//     let x = document.gnnerHTML.toLowerCase().includes(input)) {
+//             x[i].style.display="none";
+//         }etElementsByClassName('clothes');
+      
+//     for (i = 0; i < x.length; i++) { 
+//         if (!x[i].i
+//         else {
+//             x[i].style.display="flex";                 
+//         }
+//     }
+// }
